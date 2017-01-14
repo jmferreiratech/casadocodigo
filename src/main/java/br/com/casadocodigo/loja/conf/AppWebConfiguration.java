d4e825e470df6f45a9f41b310b3ff6b721875395
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-01-14 11:18:06 -0300

    Chap 15: Sending e-mails

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -18,6 +18,8 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
@@ -35,6 +37,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 @EnableWebMvc
@@ -111,5 +114,19 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         resolver.setViewResolvers(resolvers);
         resolver.setContentNegotiationManager(manager);
         return resolver;
+    }
+
+    @Bean
+    public MailSender mailSender() {
+        JavaMailSenderImpl javaMailSenderImpl = new JavaMailSenderImpl();
+        javaMailSenderImpl.setHost("smtp.gmail.com");
+        javaMailSenderImpl.setPassword("password");
+        javaMailSenderImpl.setPort(587);
+        javaMailSenderImpl.setUsername("seulogin");
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", true);
+        mailProperties.put("mail.smtp.starttls.enable", true);
+        javaMailSenderImpl.setJavaMailProperties(mailProperties);
+        return javaMailSenderImpl;
     }
 }
--- src/main/java/br/com/casadocodigo/loja/controllers/PaymentController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PaymentController.java
@@ -2,7 +2,11 @@ package br.com.casadocodigo.loja.controllers;
 
 import br.com.casadocodigo.loja.models.PaymentData;
 import br.com.casadocodigo.loja.models.ShoppingCart;
+import br.com.casadocodigo.loja.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,14 +25,17 @@ public class PaymentController {
     private ShoppingCart shoppingCart;
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private MailSender mailer;
 
     @RequestMapping(value = "checkout", method = RequestMethod.POST)
-    public Callable<ModelAndView> checkout() {
+    public Callable<ModelAndView> checkout(@AuthenticationPrincipal User user) {
         return () -> {
             BigDecimal total = shoppingCart.getTotal();
             String uriToPay = "http://book-payment.herokuapp.com/payment";
             try {
                 restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+                sendNewPurchaseMail(user);
                 return new ModelAndView("redirect:/payment/success");
             } catch (HttpClientErrorException exception) {
                 return new ModelAndView("redirect:/payment/error");
@@ -44,5 +51,14 @@ public class PaymentController {
     @RequestMapping("/error")
     public String error() {
         return "/payment/error";
+    }
+
+    private void sendNewPurchaseMail(User user) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom("compras@casadocodigo.com.br");
+        email.setTo(user.getLogin());
+        email.setSubject("Nova compra");
+        email.setText("corpo do email");
+        mailer.send(email);
     }
 }
